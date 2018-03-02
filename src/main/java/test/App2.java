5ed038c1337249b992adb20504c16985fe91a6b9
Rajesh <kintalirajesh@gmail.com>
2018-03-02 09:56:34 +0530

    first commit

---
+++ src/main/java/test/App2.java
@@ -0,0 +1,8 @@
+package test;
+
+public class App2 {
+	public String Sample() {
+		return "Hello";
+	}
+
+}
---
+++ src/test/java/test/AppTest2.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class AppTest2 {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+	}
+
+}
